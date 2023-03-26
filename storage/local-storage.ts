e4b57ceb480ce6b0d6c909d7bfc362a924eefaf4
Jatin Kumar <main.jatink@gmail.com>
2023-03-27 02:04:46 +0530

    debug(counter): testing counter

--- storage/local-storage.ts
+++ storage/local-storage.ts
@@ -1,6 +1,8 @@
 import { KeyValue } from "./key-value.ts";
 import { join } from "https://deno.land/std@0.181.0/path/mod.ts";
 
+const encode = new TextEncoder().encode;
+
 export class LocalStorage<T> extends KeyValue<T> {
   constructor(private _path: string = join(Deno.cwd(), "localstorage.json")) {
     super();
@@ -9,7 +11,7 @@ export class LocalStorage<T> extends KeyValue<T> {
         this.data = JSON.parse(await Deno.readTextFile(this._path));
       } catch (err) {
         if (err instanceof Deno.errors.NotFound)
-          Deno.writeTextFileSync(this._path, JSON.stringify(this.data));
+          Deno.writeFile(this._path, encode(JSON.stringify(this.data)));
         else throw err;
       }
     };
@@ -23,6 +25,9 @@ export class LocalStorage<T> extends KeyValue<T> {
     replacer?: (this: unknown, key: string, value: unknown) => unknown,
     space?: string | number
   ) {
-    Deno.writeTextFile(this._path, JSON.stringify(this.data, replacer, space));
+    Deno.writeFile(
+      this._path,
+      encode(JSON.stringify(this.data, replacer, space))
+    );
   }
 }
