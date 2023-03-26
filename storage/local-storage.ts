2e92f9e0ee3868c2df46ee7fc5b13aea5c187336
Jatin Kumar <main.jatink@gmail.com>
2023-03-27 02:21:56 +0530

    debug(counter): testing counter

--- storage/local-storage.ts
+++ storage/local-storage.ts
@@ -1,18 +1,18 @@
-import { KeyValue, keyValue } from "./key-value.ts";
+import { KeyValue } from "./key-value.ts";
 import { join } from "https://deno.land/std@0.181.0/path/mod.ts";
-import { readJson, writeJson } from "https://deno.land/x/jsonfile@1.0.0/mod.ts";
-
-const encode = new TextEncoder().encode;
 
 export class LocalStorage<T> extends KeyValue<T> {
   constructor(private _path: string = join(Deno.cwd(), "localstorage.json")) {
     super();
     const read = async () => {
       try {
-        this.data = (await readJson(this._path)) as keyValue<T>;
+        this.data = JSON.parse(await Deno.readTextFile(this._path));
       } catch (err) {
         if (err instanceof Deno.errors.NotFound)
-          Deno.writeFile(this._path, encode(JSON.stringify(this.data)));
+          Deno.writeFile(
+            this._path,
+            new TextEncoder().encode(JSON.stringify(this.data))
+          );
         else throw err;
       }
     };
@@ -22,7 +22,13 @@ export class LocalStorage<T> extends KeyValue<T> {
    * Sava data from memory to disk. Use it only when program end. Don't overuse it.
    */
 
-  save() {
-    writeJson(this._path, this.data);
+  save(
+    replacer?: (this: unknown, key: string, value: unknown) => unknown,
+    space?: string | number
+  ) {
+    Deno.writeFile(
+      this._path,
+      new TextEncoder().encode(JSON.stringify(this.data, replacer, space))
+    );
   }
 }
