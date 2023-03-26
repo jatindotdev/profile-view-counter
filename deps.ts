3922663b740119a98bf0742acc7fdd20db2ce0b3
Jatin Kumar <main.jatink@gmail.com>
2023-03-26 18:55:57 +0530

    feat(counter): add counter

---
+++ deps.ts
@@ -0,0 +1,2 @@
+export { serve } from "https://deno.land/std@0.181.0/http/server.ts";
+export { default as satori } from "npm:satori";
---
+++ main.tsx
@@ -0,0 +1,66 @@
+/* @jsxImportSource https://esm.sh/react@18.2.0 */
+
+import { satori, serve } from "./deps.ts";
+
+const opts = {
+  port: 3000,
+};
+
+const markup = (count: string) => (
+  <div
+    style={{
+      all: "unset",
+      display: "flex",
+      justifyContent: "center",
+      alignItems: "center",
+      backgroundColor: "#fd5d5d",
+      padding: "0.5px 2px 0.5px",
+      textDecoration: "none",
+      color: "#fff",
+      borderRadius: "1.5px",
+      fontSize: "4px",
+    }}>
+    {count} views
+  </div>
+);
+
+async function handler(req: Request) {
+  const url = new URL(req.url);
+
+  console.log({ url });
+
+  const svg = await satori(markup("500"), {
+    height: 200,
+    fonts: [
+      {
+        name: "Outfit",
+        data: Deno.readFileSync("./fonts/outfit-regular.ttf")
+          .buffer as ArrayBuffer,
+        style: "normal",
+        weight: 400,
+      },
+      {
+        name: "Outfit",
+        data: Deno.readFileSync("./fonts/outfit-medium.ttf")
+          .buffer as ArrayBuffer,
+        style: "normal",
+        weight: 600,
+      },
+      {
+        name: "Outfit",
+        data: Deno.readFileSync("./fonts/outfit-bold.ttf")
+          .buffer as ArrayBuffer,
+        style: "normal",
+        weight: 800,
+      },
+    ],
+  });
+
+  return new Response(svg, {
+    headers: {
+      "content-type": "image/svg+xml",
+    },
+  });
+}
+
+serve(handler, opts);
