460a6f57ae85a979c5818831b009088f5aca7e3d
Jatin Kumar <main.jatink@gmail.com>
2023-03-26 20:32:33 +0530

    fix(font): fix font

--- deps.ts
+++ deps.ts
@@ -1,9 +1,5 @@
-import cacheDir from "https://deno.land/x/cache_dir@0.2.0/mod.ts";
-
 export { serve } from "https://deno.land/std@0.181.0/http/server.ts";
 export { default as satori, init } from "https://esm.sh/satori@0.4.4/wasm";
 export { default as initYoga } from "https://esm.sh/yoga-wasm-web@0.3.3";
 
-export const wasm = await Deno.readFile(
-  `${cacheDir()}/deno/npm/registry.npmjs.org/yoga-wasm-web/0.3.3/dist/yoga.wasm`
-);
+export const wasm = await Deno.readFile(`./wasm/yoga.wasm`);
