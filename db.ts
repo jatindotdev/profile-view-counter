313806887e75fa4824d8ac8b2b1fac00f29622f9
Jatin Kumar <main.jatink@gmail.com>
2023-03-26 23:22:03 +0530

    debug(counter): testing counter

--- db.ts
+++ db.ts
@@ -1,29 +1,30 @@
+import { openKVDb } from "https://deno.land/x/kv_sqlite@v0.1.7/mod.ts";
+
+const db = openKVDb("db.sqlite3");
+
 // Get the count for a specific item by name
 export function getCount(name: string): string {
-  return (localStorage.getItem(name) ?? 0).toString();
+  return (db.get(name) ?? 0).toString();
 }
 
 // Increment the count for a specific item by name
 export function incrementCount(name: string) {
   const count = parseInt(getCount(name)) + 1;
-  localStorage.setItem(name, count.toString());
+  db.set(name, count.toString());
 }
 
 // Get all items in the database
 export function getAll() {
   const items = [];
-  for (let i = 0; i < localStorage.length; i++) {
-    const key = localStorage.key(i);
-    if (key) {
-      items.push({
-        name: key,
-        count: getCount(key),
-      });
-    }
+  for (const key in db.keys) {
+    items.push({
+      name: key,
+      count: db.get(key),
+    });
   }
   return items;
 }
 
 export function eraseAll() {
-  localStorage.clear();
+  db.clear();
 }
