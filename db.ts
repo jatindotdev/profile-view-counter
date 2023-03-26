f61f577fcc91b3cf20c8b9802045d2f633209822
Jatin Kumar <main.jatink@gmail.com>
2023-03-26 23:06:49 +0530

    debug(counter): testing counter

--- db.ts
+++ db.ts
@@ -1,36 +1,29 @@
-import { DB, Row } from "https://deno.land/x/sqlite@v3.7.0/mod.ts";
-
-const db = new DB("items.db");
-
 // Get the count for a specific item by name
-export function getCount(name: string): number {
-  const item = db.query("SELECT count FROM items WHERE name = ?", [name]);
-  if (item.length === 0) {
-    throw new Error(`Item with name '${name}' not found`);
-  }
-  return item[0][0] as number;
+export function getCount(name: string): string {
+  return (localStorage.getItem(name) ?? 0).toString();
 }
 
 // Increment the count for a specific item by name
 export function incrementCount(name: string) {
-  db.query("INSERT OR IGNORE INTO items (name, count) VALUES (?, 0)", [name]);
-  db.query("UPDATE items SET count = count + 1 WHERE name = ?", [name]);
+  const count = parseInt(getCount(name)) + 1;
+  localStorage.setItem(name, count.toString());
 }
 
 // Get all items in the database
-export function getAll(): Row[] {
-  const items: Row[] = db.query("SELECT id, name, count FROM items");
+export function getAll() {
+  const items = [];
+  for (let i = 0; i < localStorage.length; i++) {
+    const key = localStorage.key(i);
+    if (key) {
+      items.push({
+        name: key,
+        count: getCount(key),
+      });
+    }
+  }
   return items;
 }
 
 export function eraseAll() {
-  db.query("DELETE FROM items");
+  localStorage.clear();
 }
-
-db.query(`
-    CREATE TABLE IF NOT EXISTS items (
-      id INTEGER PRIMARY KEY AUTOINCREMENT,
-      name TEXT UNIQUE,
-      count UNSIGNED INTEGER
-    )
-`);
