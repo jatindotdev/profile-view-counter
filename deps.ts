05bf29fcdb60594ca34d1f5dcc56d69430bf81a4
Jatin Kumar <main.jatink@gmail.com>
2023-03-26 19:15:43 +0530

    chore(deps): update dependency url

--- deps.ts
+++ deps.ts
@@ -1,2 +1,2 @@
 export { serve } from "https://deno.land/std@0.181.0/http/server.ts";
-export { default as satori } from "npm:satori";
+export { default as satori } from "https://esm.sh/satori@0.4.4";
